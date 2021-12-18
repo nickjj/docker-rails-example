fc06671da24960a6350b49446bbe9b6ff0be2ed8
Nick Janetakis <nick.janetakis@gmail.com>
2021-12-18 14:32:23 -0500

    Move Action Cable configuration in 1 spot

--- config/application.rb
+++ config/application.rb
@@ -26,10 +26,11 @@ module Hello
     # Set Sidekiq as the back-end for Active Job.
     config.active_job.queue_adapter = :sidekiq
 
-    # Separate Action Cable into its own process.
+    # Mount Action Cable outside the main process or domain.
+    config.action_cable.mount_path = nil
     config.action_cable.url = ENV.fetch("ACTION_CABLE_FRONTEND_URL") { "ws://localhost:28080" }
 
-    # Action Cable will only allow connections from these domains.
+    # Only allow connections to Action Cable from these domains.
     origins = ENV.fetch("ACTION_CABLE_ALLOWED_REQUEST_ORIGINS") { "http:\/\/localhost*" }.split(",")
     origins.map! { |url| /#{url}/ }
     config.action_cable.allowed_request_origins = origins
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,11 +40,6 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Mount Action Cable outside main process or domain.
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = "wss://example.com/cable"
-  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
-
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
