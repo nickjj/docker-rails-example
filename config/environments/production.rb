52033c0d3a1a7560908aa86f1b2445f9ef2783b6
Nick Janetakis <nick.janetakis@gmail.com>
2021-12-18 14:34:20 -0500

    Remove cache back-end related comments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,13 +50,6 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
-  # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
-
-  # Use a real queuing backend for Active Job (and separate queues per environment).
-  # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "hello_production"
-
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
